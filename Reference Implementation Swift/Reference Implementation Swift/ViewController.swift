51f387b29eb578d67d7163c395a5aaf2762f0214
Anthony Picciano <anthony.picciano@gmail.com>
2016-08-30 12:20:29 -0600

    Include initialization and debug flag.

--- Reference Implementation Swift/Reference Implementation Swift/ViewController.swift
+++ Reference Implementation Swift/Reference Implementation Swift/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     
     // Retain this instance
     var mapViewDelegate = MapViewDelegate()
+    
+    override func viewDidLoad() {
+        TallyGoKit.initialize(withAccessToken: "YOUR ACCESS TOKEN")
+        TallyGoKit.allowsDebug = true
+    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let viewController = segue.destination as? TGViewController {
