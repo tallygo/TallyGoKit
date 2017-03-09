abeeb02318552c0ab43bd4596ee1895c92289dd1
Anthony Picciano <picciano@Anthonys-MacBook-Pro-2.local>
2017-03-08 18:57:12 -0700

    Finishing touches on documentation

--- TallyGoKit.framework/GenDocs.sh
+++ TallyGoKit.framework/GenDocs.sh
@@ -1,2 +1,2 @@
 rm -rf Documentation
-jazzy --podspec TallyGoKit.podspec --swift-version 3.0.2 --output Documentation --author "TallyGo LLC"
+jazzy --podspec TallyGoKit.podspec --swift-version 3.0.2 --output Documentation --author "TallyGo LLC" --readme README.md
