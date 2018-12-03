ab030414b5c3283e6f4207321cfe6256a3f632bf
David Deller <david@tallygo.com>
2018-12-03 15:38:39 -0800

    Update cocoapods and makezip script

--- makezip.sh
+++ makezip.sh
@@ -2,12 +2,13 @@ zip -r ~/Desktop/TallyGoKit.zip LICENSE TallyGoKit.framework
 
 cd "Reference Implementation Swift"
 pod install
+pod update
 cd ..
 
 cp -a "Reference Implementation Swift/Pods/Mapbox-iOS-SDK" .
-cp -a "Reference Implementation Swift/Pods/SwiftyJSON" .
+cp -a "Reference Implementation Swift/Pods/KissXML" .
 
-zip -r ~/Desktop/TallyGoKit-With-Dependencies.zip LICENSE TallyGoKit.framework Mapbox-iOS-SDK SwiftyJSON
+zip -r ~/Desktop/TallyGoKit-With-Dependencies.zip LICENSE TallyGoKit.framework Mapbox-iOS-SDK KissXML
 
 rm -rf Mapbox-iOS-SDK
-rm -rf SwiftyJSON
+rm -rf KissXML
